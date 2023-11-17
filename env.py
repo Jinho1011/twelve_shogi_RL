fb0280b5af2240ba7d7cc8befb6be2977fd1a9e6
Jeon <gino9940@gmail.com>
2023-11-17 11:21:51 +0900

    feat: add pygame env

--- env.py
+++ env.py
@@ -1,3 +1,5 @@
+# -*- coding:utf-8 -*-
+
 import pygame as pg
 import numpy as np
 
@@ -11,56 +13,61 @@ line_color = (0, 0, 0)  # Black lines for the grid
 white_stone = (255, 255, 255)
 black_stone = (0, 0, 0)
 
-class Connect6EnvAdversarial():
+
+class TwelveShogi():
     def __init__(self) -> None:
-        self.state = {0 : np.zeros((row_size, col_size), dtype=np.int32), 1 : np.zeros((row_size, col_size), dtype=np.int32)}
-        self.reward_dict = {"victory" : 10.0, "defeat" : -10.0, "step" : 0.0001, "overlap" : -0.1}
+        self.state = {0: np.zeros((row_size, col_size), dtype=np.int32), 1: np.zeros(
+            (row_size, col_size), dtype=np.int32)}
+        self.reward_dict = {"victory": 10.0, "defeat": -
+                            10.0, "step": 0.0001, "overlap": -0.1}
         self.screen = None
 
-
     def reset(self):
-        self.state = {0 : np.zeros((row_size, col_size), dtype=np.int32), 1 : np.zeros((row_size, col_size), dtype=np.int32)}
+        self.state = {0: np.zeros((row_size, col_size), dtype=np.int32), 1: np.zeros(
+            (row_size, col_size), dtype=np.int32)}
         obs = self.state[0]
         return obs
 
-    
     def finish_check(self) -> bool:
         for i in range(0, row_size):
             for j in range(0, col_size):
                 for k in [1, -1]:
-                    try: 
+                    try:
                         if (k == self.state[0][i, j] == self.state[0][i + 1, j] == self.state[0][i + 2, j] == self.state[0][i + 3, j] == self.state[0][i + 4, j] == self.state[0][i + 5, j]):
                             return k
-                    except: pass
+                    except:
+                        pass
 
         for j in range(0, row_size):
             for i in range(0, col_size):
                 for k in [1, -1]:
-                    try: 
+                    try:
                         if (k == self.state[0][i, j] == self.state[0][i, j + 1] == self.state[0][i, j + 2] == self.state[0][i, j + 3] == self.state[0][i, j + 4] == self.state[0][i, j + 5]):
                             return k
-                    except: pass
+                    except:
+                        pass
 
         for i in range(0, row_size):
             for j in range(0, col_size):
                 for k in [1, -1]:
-                    try: 
+                    try:
                         if (k == self.state[0][i, j] == self.state[0][i + 1, j + 1] == self.state[0][i + 2, j + 2] == self.state[0][i + 3, j + 3] == self.state[0][i + 4, j + 4] == self.state[0][i + 5, j + 5]):
                             return k
-                    except: pass
+                    except:
+                        pass
 
         for i in range(0, row_size):
             for j in range(0, col_size):
                 for k in [1, -1]:
-                    try: 
+                    try:
                         if (k == self.state[0][i, j] == self.state[0][i - 1, j + 1] == self.state[0][i - 2, j + 2] == self.stat[0][i - 3, j + 3] == self.state[0][i - 4, j + 4] == self.state[0][i - 5, j + 5]):
                             return k
-                    except: pass
+                    except:
+                        pass
 
         return 0
 
-
-    def heuristic(self, turn : int, layer : int=0):
+    def heuristic(self, turn: int, layer: int = 0):
         """
         returns heuristic information about states
         turn : turn of agnet (0 or 1)
@@ -79,12 +86,16 @@ class Connect6EnvAdversarial():
                 count1 = 0
                 count2 = 0
                 for k in [-5, -4, -3, -2, -1, 1, 2, 3, 4, 5]:
-                    try: 
-                        if (1 == self.state[turn][i + k, j]): count1 += 1 
-                    except: pass
-                    try: 
-                        if (-1 == self.state[turn][i + k, j]) : count2 += 1 
-                    except: pass
+                    try:
+                        if (1 == self.state[turn][i + k, j]):
+                            count1 += 1
+                    except:
+                        pass
+                    try:
+                        if (-1 == self.state[turn][i + k, j]):
+                            count2 += 1
+                    except:
+                        pass
 
                 hlayer1[i, j] += count1
                 hlayer2[i, j] += count2
@@ -93,12 +104,16 @@ class Connect6EnvAdversarial():
                 count1 = 0
                 count2 = 0
                 for k in [-5, -4, -3, -2, -1, 1, 2, 3, 4, 5]:
-                    try: 
-                        if (1 == self.state[turn][i, j + k]): count1 += 1 
-                    except: pass
-                    try: 
-                        if (-1 == self.state[turn][i, j + k]) : count2 += 1 
-                    except: pass
+                    try:
+                        if (1 == self.state[turn][i, j + k]):
+                            count1 += 1
+                    except:
+                        pass
+                    try:
+                        if (-1 == self.state[turn][i, j + k]):
+                            count2 += 1
+                    except:
+                        pass
 
                 hlayer1[i, j] += count1
                 hlayer2[i, j] += count2
@@ -107,41 +122,49 @@ class Connect6EnvAdversarial():
                 count1 = 0
                 count2 = 0
                 for k in [-5, -4, -3, -2, -1, 1, 2, 3, 4, 5]:
-                    try: 
-                        if (1 == self.state[turn][i + k, j - k]): count1 += 1
-                    except: pass
-                    try: 
-                        if (-1 == self.state[turn][i + k, j - k]) : count2 += 1 
-                    except: pass
+                    try:
+                        if (1 == self.state[turn][i + k, j - k]):
+                            count1 += 1
+                    except:
+                        pass
+                    try:
+                        if (-1 == self.state[turn][i + k, j - k]):
+                            count2 += 1
+                    except:
+                        pass
 
                 hlayer1[i, j] += count1
                 hlayer2[i, j] += count2
-
 
                 # diag upper right to bottom left /
                 count1 = 0
                 count2 = 0
                 for k in [-5, -4, -3, -2, -1, 1, 2, 3, 4, 5]:
-                    try: 
-                        if (1 == self.state[turn][i - k, j + k]): count1 += 1
-                    except: pass
-                    try: 
-                        if (-1 == self.state[turn][i - k, j + k]) : count2 += 1 
-                    except: pass
+                    try:
+                        if (1 == self.state[turn][i - k, j + k]):
+                            count1 += 1
+                    except:
+                        pass
+                    try:
+                        if (-1 == self.state[turn][i - k, j + k]):
+                            count2 += 1
+                    except:
+                        pass
 
                 hlayer1[i, j] += count1
                 hlayer2[i, j] += count2
 
-        if layer == 1: return hlayer1
-        elif layer == -1: return hlayer2
-        else: return hlayer1, hlayer2
+        if layer == 1:
+            return hlayer1
+        elif layer == -1:
+            return hlayer2
+        else:
+            return hlayer1, hlayer2
 
-
-    def get_state(self, turn : int):
+    def get_state(self, turn: int):
         return self.state[turn]
 
-    
-    def put_check(self, action : int, turn : int):
+    def put_check(self, action: int, turn: int):
         idx = (action // col_size, action % row_size)
 
         if self.state[turn][idx] != 0.0:
@@ -149,30 +172,28 @@ class Connect6EnvAdversarial():
 
         return True
 
-
-    def step(self, action : int, turn : int):
+    def step(self, action: int, turn: int):
         idx = (action // col_size, action % row_size)
-
 
         if (turn == 0):
             if self.state[0][idx] == 0.0:
                 self.state[0][idx] = 1.0
                 self.state[1][idx] = -1.0
                 reward = self.reward_dict["step"]
-                info = {"pass" : True}
+                info = {"pass": True}
             else:
                 reward = self.reward_dict["overlap"]
-                info = {"pass" : False}
+                info = {"pass": False}
 
         else:
             if self.state[1][idx] == 0.0:
                 self.state[1][idx] = 1.0
                 self.state[0][idx] = -1.0
                 reward = self.reward_dict["step"]
-                info = {"pass" : True}
+                info = {"pass": True}
             else:
                 reward = self.reward_dict["overlap"]
-                info = {"pass" : False}
+                info = {"pass": False}
 
         next_obs = self.get_state(turn)
 
@@ -189,7 +210,6 @@ class Connect6EnvAdversarial():
 
         return next_obs, reward, done, info
 
-
     def seton(self, action, turn):
         idx = (action // col_size, action % row_size)
         self.state[turn][idx] = 1.0
@@ -198,32 +218,44 @@ class Connect6EnvAdversarial():
     def update(self):
         for k in self.state:
             print(k)
-    
+
     def render(self):
-        radius = grid_size // 2 - 2 
-        
+        radius = grid_size // 2 - 2
+
         if not self.screen:
             pg.init()
-            self.screen = pg.display.set_mode((row_size * grid_size, col_size * grid_size))
+            self.screen = pg.display.set_mode(
+                (row_size * grid_size, col_size * grid_size))
             pg.display.set_caption('Shogi Game')
-    
-        
+            self.font = pg.font.SysFont("AppleSDGothicNeo", 40, True, False)
+
         self.screen.fill(board_color)
 
         # Draw the grid
         for i in range(row_size):
             for j in range(col_size):
-                rect = pg.Rect(i * grid_size, j * grid_size, grid_size, grid_size)
+                rect = pg.Rect(i * grid_size, j * grid_size,
+                               grid_size, grid_size)
                 pg.draw.rect(self.screen, line_color, rect, 1)
 
         # Draw the stones
         for i in range(row_size):
             for j in range(col_size):
-                center = (j * grid_size + grid_size // 2, i * grid_size + grid_size // 2)
-                if self.state[0][i][j] == 1:  # Player 1 stone
+                center = (j * grid_size + grid_size // 2,
+                          i * grid_size + grid_size // 2)
+
+                if self.state[0][i][j] == 1:  # Player 1's 장
                     pg.draw.circle(self.screen, black_stone, center, radius)
+                    text_surface = self.font.render(
+                        '장', True, white_stone)  # White text on black stone
+                    text_rect = text_surface.get_rect(center=center)
+                    self.screen.blit(text_surface, text_rect)
                 elif self.state[1][i][j] == 1:  # Player 2 stone
                     pg.draw.circle(self.screen, white_stone, center, radius)
+                    text_surface = self.font.render(
+                        '장', True, black_stone)  # Black text on white stone
+                    text_rect = text_surface.get_rect(center=center)
+                    self.screen.blit(text_surface, text_rect)
 
         # Update the display
         pg.display.flip()
