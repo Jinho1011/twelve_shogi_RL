42793035838ff474c4caca64aaffd243a482fac9
Jeon <gino9940@gmail.com>
2023-11-17 11:22:07 +0900

    fix: rename to TwelveShogi

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-from env import Connect6EnvAdversarial
+# -*- coding:utf-8 -*-
+
+from env import TwelveShogi
 import pygame as pg
 import random
 import time
 
-env = Connect6EnvAdversarial()
+env = TwelveShogi()
 
 obs = env.reset()
 done = False
@@ -12,16 +14,16 @@ step = 0
 cum_reward = 0.0
 
 pg.init()
-pg.display.set_caption('Connect6 Game')
+pg.display.set_caption('TwelveShogi Game')
 clock = pg.time.Clock()
 target = 0
-    
+
 
 while not done:
     for event in pg.event.get():
         if event.type == pg.QUIT:
             done = True
-    
+
     action = random.randint(0, 3 * 4 - 1)
     next_obs, reward, done, info = env.step(action, target)
 
@@ -35,9 +37,11 @@ while not done:
 
     # Cap the frame rate
     clock.tick(60)
-    
-    if target == 0: target = 1
-    else: target = 0
+
+    if target == 0:
+        target = 1
+    else:
+        target = 0
 
 done = False
 while not done:
